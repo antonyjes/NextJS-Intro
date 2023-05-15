db6bf148080589673c66c00b0932e147062134b0
antonyjes <72809149@continental.edu.pe>
2023-05-15 12:19:07 -0500

    Nav - admin

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button className="bg-white p-2 px-4 rounded-lg" onClick={() => signIn('google')}>
-            Login with Google
-          </button>
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <Image
+            src={session?.user?.image}
+            alt="user-img"
+            width={24}
+            height={24}
+          />
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-
-  return <div>Logged in as {session.user.email}</div>;
+    </Layout>
+  );
 }
---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+const Products = () => {
+    return(
+        <Layout>
+            Products page
+        </Layout>
+    )
+}
+
+export default Products;
